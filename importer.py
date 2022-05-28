754e57401e256f819d20d403059634acf96042d8
Oguz Han Asnaz <github@lostgeek.de>
2022-05-28 13:56:58 +0200

    Updated importer for Midnight Sun

--- importer.py
+++ importer.py
@@ -8,72 +8,96 @@ import sys
 import re
 import getpass
 from github import Github
+import time
 
-class Card:
-    def __init__(self, name=None, faction=None, type=None, text=None):
-        self.name = name
-        self.faction = faction
-        self.type = type
-        self.text = text
+def issue_title(c):
+    return "{0[Faction]} - {0[Card Name]}".format(c)
 
-    def issue_title(self):
-        return "{0.faction} - {0.name}".format(self)
+def issue_body(c, hs):
+    headers = hs[:] # pass by value
+    out = []
+    out.append("### {0[Card Name]}")
+    out.append("{0[Faction]} - {0[Type]}")
 
-    def issue_body(self):
-        return """### {0.name}
-{0.faction} - {0.type}
-> {1}
+    headers.remove('Card Name')
+    headers.remove('Faction')
+    headers.remove('Type')
+    headers.remove('Card Text')
 
-### Progress
-- [ ] Implement functionality
-- [ ] Write tests""".format(self, "\n".join(self.text))
+    for h in headers:
+        if h in c.keys() and c[h]:
+            out.append(h+': {0['+h+']}')
 
-def read_file(fn):
+    out.append("")
+    out.append("> {0[Card Text]}")
+    out.append("")
+
+    out.append("### Progress")
+    out.append("- [ ] Implement functionality")
+    out.append("- [ ] Write tests")
+
+    return '\n'.join(out).format(c);
+
+def get_headers(fn):
     cards = []
     with open(fn, 'r') as f:
-        curr_card = Card()
-        for l in f:
-            match = re.match(r'^(.+?)\t(.+?)\t(.+?)\t"(.+?)$', l)
-            if match:
-                cards.append(curr_card)
-                curr_card = Card(name=match.group(2), faction=match.group(1), type=match.group(3), text=[match.group(4)])
+        curr_card = {}
+        lines = [line.rstrip() for line in f]
+        return lines[0].split('\t')
+
+def read_file(fn, headers):
+    cards = []
+    with open(fn, 'r') as f:
+        curr_card = {}
+        lines = [line.rstrip() for line in f]
+        for l in lines[1:]:
+            if l.count('\t') < 12: # this must be another line of card text instead of a new card
+                curr_card['Card Text'] += '\n' + l
             else:
-                l = l.strip()
-                if l[-1] == '"':
-                    l = l[:-1]
-                curr_card.text.append(l)
-        cards.append(curr_card)
+                cards.append(curr_card)
+                values = l.split('\t');
+                curr_card = dict(zip(headers, values))
+    cards.append(curr_card)
     return cards[1:]
 
 def main():
+    print("Determining headers...")
+    headers = get_headers(sys.argv[1])
+    print()
+
     print("Reading cards...")
-    cards = read_file(sys.argv[1])
+    cards = read_file(sys.argv[1], headers)
     print()
 
-    print("Loaded cards:")
+    print("Loaded %d cards:"%len(cards))
     for c in cards:
-        print(c.issue_title())
+        print(issue_title(c))
     print()
 
-    username = input("Enter Github username: ")
-    password = getpass.getpass("Enter Github password: ")
+    #  username = input("Enter Github username: ")
+    #  password = getpass.getpass("Enter Github password: ")
+    #  g = Github(username, password)
 
-    g = Github(username, password)
+    access_token = "INSERT_TOKEN_HERE" # TODO: change this
+    g = Github(access_token)
 
-    print()
+    print(g)
     reponame = input("Enter repository name (default: NoahTheDuke/netrunner-future): ")
     if not reponame:
         reponame = "NoahTheDuke/netrunner-future"
+        print("Chosing %s"%reponame)
+
     repo = g.get_repo(reponame)
 
     print("Using repository: %s (%d open issues)" % (repo.name, repo.open_issues_count))
 
     input("Ready to create %d issues for new cards. Press ENTER to continue..." % len(cards))
 
-    for c in cards:
-        print("Creating issue for %s" % c.name, end='... ')
-        repo.create_issue(title=c.issue_title(), body=c.issue_body(), labels=["New Card"])
+    for c in cards[40:]:
+        print("Creating issue for {0[Card Name]}".format(c), end='... ')
+        repo.create_issue(title=issue_title(c), body=issue_body(c, headers), labels=["New Card"])
         print("done.")
+        time.sleep(5) # get around ratelimiting
 
 if __name__ == "__main__":
     main()
