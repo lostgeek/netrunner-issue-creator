30887d070c12d49a09c8287e98a325cf58dbe644
Oguz Han Asnaz <github@lostgeek.de>
2022-11-20 13:27:44 +0100

    Updated importer for Parhelion

--- importer.py
+++ importer.py
@@ -5,28 +5,19 @@
 # Copy the content of the Google document into a textfile. Hope that nothing changed since Uprising. Follow the instructions to create issues in the netrunner repository.
 
 import sys
+import csv
 import re
 import getpass
 from github import Github
 import time
 
 def issue_title(c):
-    return "{0[Faction]} - {0[Card Name]}".format(c)
+    return "{0[Set Number]} - {0[Faction]} - {0[Card Name (English - For Translation)]}".format(c)
 
-def issue_body(c, hs):
-    headers = hs[:] # pass by value
+def issue_body(c):
     out = []
-    out.append("### {0[Card Name]}")
+    out.append("### {0[Card Name (English - For Translation)]}")
     out.append("{0[Faction]} - {0[Type]}")
-
-    headers.remove('Card Name')
-    headers.remove('Faction')
-    headers.remove('Type')
-    headers.remove('Card Text')
-
-    for h in headers:
-        if h in c.keys() and c[h]:
-            out.append(h+': {0['+h+']}')
 
     out.append("")
     out.append("> {0[Card Text]}")
@@ -38,35 +29,16 @@ def issue_body(c, hs):
 
     return '\n'.join(out).format(c);
 
-def get_headers(fn):
+def read_file(fn):
     cards = []
     with open(fn, 'r') as f:
-        curr_card = {}
-        lines = [line.rstrip() for line in f]
-        return lines[0].split('\t')
-
-def read_file(fn, headers):
-    cards = []
-    with open(fn, 'r') as f:
-        curr_card = {}
-        lines = [line.rstrip() for line in f]
-        for l in lines[1:]:
-            if l.count('\t') < 12: # this must be another line of card text instead of a new card
-                curr_card['Card Text'] += '\n' + l
-            else:
-                cards.append(curr_card)
-                values = l.split('\t');
-                curr_card = dict(zip(headers, values))
-    cards.append(curr_card)
-    return cards[1:]
+        r = csv.DictReader(f, delimiter=',')
+        cards = list(r)
+    return cards
 
 def main():
-    print("Determining headers...")
-    headers = get_headers(sys.argv[1])
-    print()
-
     print("Reading cards...")
-    cards = read_file(sys.argv[1], headers)
+    cards = read_file(sys.argv[1])
     print()
 
     print("Loaded %d cards:"%len(cards))
@@ -74,18 +46,13 @@ def main():
         print(issue_title(c))
     print()
 
-    #  username = input("Enter Github username: ")
-    #  password = getpass.getpass("Enter Github password: ")
-    #  g = Github(username, password)
-
     access_token = "INSERT_TOKEN_HERE" # TODO: change this
     g = Github(access_token)
 
-    print(g)
     reponame = input("Enter repository name (default: NoahTheDuke/netrunner-future): ")
     if not reponame:
         reponame = "NoahTheDuke/netrunner-future"
-        print("Chosing %s"%reponame)
+        print("Choosing %s"%reponame)
 
     repo = g.get_repo(reponame)
 
@@ -93,11 +60,13 @@ def main():
 
     input("Ready to create %d issues for new cards. Press ENTER to continue..." % len(cards))
 
-    for c in cards[40:]:
-        print("Creating issue for {0[Card Name]}".format(c), end='... ')
-        repo.create_issue(title=issue_title(c), body=issue_body(c, headers), labels=["New Card"])
+    for c in cards:
+        print("Creating issue for {0[Card Name (English - For Translation)]}".format(c), end='... ')
+        repo.create_issue(title=issue_title(c), body=issue_body(c), labels=["New Card"])
+        #  print("Would create: {0}".format(issue_title(c)))
+        print(issue_body(c))
         print("done.")
-        time.sleep(5) # get around ratelimiting
+        time.sleep(3) # get around ratelimiting
 
 if __name__ == "__main__":
     main()
